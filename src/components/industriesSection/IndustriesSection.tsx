3dc1667ac982b79abbed5372c60610c1f0131dde
Aleksey Ponomarev <mail.ponomarev@yandex.ru>
2020-06-26 10:54:04 +0300

    build(components): finished idustriesSection

--- src/components/industriesSection/IndustriesSection.tsx
+++ src/components/industriesSection/IndustriesSection.tsx
@@ -4,6 +4,8 @@ import theme from '../ui/Theme'
 import DivMargin from '../ui/DivMargin'
 import IndusriesTitle from './sectionComponent/IndusriesTitle'
 import IndustriBlock from './sectionComponent/IndustriBlock'
+import { SvgStyle } from '../ui/svg/SvgStyle'
+import { WaveBig } from '../ui/svg/Wave'
 
 const SectionStyle = styled.section`
 background-color: ${theme.colors.beige}`
@@ -12,7 +14,15 @@ const SectionBlock = styled.div`
 width: 1335px;
 margin: 0 auto;
 display: flex;
-align-items: center;`
+align-items: center;
+position: relative;
+z-index: 1;`
+
+const WaveBlock = styled.div`
+position: absolute;
+top: 160px;
+left: 303px;
+z-index: 0;`
 
 const IndustriesSection = ()=>{
   return(
@@ -22,6 +32,11 @@ const IndustriesSection = ()=>{
         <IndusriesTitle/>
         <DivMargin width='50px'/>
         <IndustriBlock/>
+        <WaveBlock>
+          <SvgStyle width='810px' height='289px' fill='none'>
+            {WaveBig}
+          </SvgStyle>
+        </WaveBlock>
       </SectionBlock>
       <DivMargin height='100px'/>
     </SectionStyle>
