8a8f9a941e3a1692b1b50abf5ff6f46b7a0d2be6
Aleksey Ponomarev <mail.ponomarev@yandex.ru>
2020-07-02 16:13:48 +0300

    fix(hero): add waves without z-index

---
+++ site/ui/hero/WaveFormTop.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled'
+
+
+export const WaveFormTop = styled.div(() => ({
+  width: '600px',
+  height: '600px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundImage: `url('site/ui/svg/Wave.svg');`,
+  backgroundSize: '285px',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'top right'
+}))
