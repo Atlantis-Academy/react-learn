7a4f60c17210abad643135a757a053d99468d764
Aleksey Ponomarev <mail.ponomarev@yandex.ru>
2020-07-07 15:36:25 +0300

    fix(bullets): text, section

--- site/lending/Bullets.tsx
+++ site/lending/Bullets.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Tabs from '../ui/Tabs'
 import DivMargin from '../ui/layout/DivMargin'
 import { Box } from '../ui/layout/Box'
-import { Svg1, Svg2, Svg3 } from '../ui/svg/SvgAdventag'
-import { Title } from '../ui/bullets/Title'
-import textComponent from '../ui/text/TextComponents'
-import { TextStyleTabs } from '../ui/bullets/Text'
+import { Speed } from '../ui/bullets/Speed'
+import { Text } from '../ui/text/Text'
+import { FormattedMessage } from 'react-intl'
+import { Pluses } from '../ui/bullets/Pluses'
+import { Cost } from '../ui/bullets/Cost'
 
 
 const Bullets = ()=>{
@@ -15,37 +16,37 @@ const Bullets = ()=>{
       <DivMargin height='80px'/>
       <Box width={'1120px'} display={'flex'} margin={'0 auto'} justifyContent={'space-between'}>
         <Box width={'280px'}>
-          <Svg1 />
+          <Speed />
           <DivMargin height='30px'/>
-          <Title>
-            {textComponent.adventages.title[0]}
-          </Title>
+          <Text themeText={'sectionTitle'}>
+            <FormattedMessage id={'bullets.speed.title'}/>
+          </Text>
           <DivMargin height='20px'/>
-          <TextStyleTabs>
-            {textComponent.adventages.text[0]}
-          </TextStyleTabs>
+          <Text size={'18px'} lineHeight={'25px'}>
+            <FormattedMessage id={'bullets.speed.descr'}/>
+          </Text>
         </Box>
         <Box width={'280px'}>
-          <Svg2 />
+          <Pluses />
           <DivMargin height='30px'/>
-          <Title>
-            {textComponent.adventages.title[1]}
-          </Title>
+          <Text themeText={'sectionTitle'}>
+            <FormattedMessage id={'bullets.pluses.title'}/>
+          </Text>
           <DivMargin height='20px'/>
-          <TextStyleTabs>
-            {textComponent.adventages.text[1]}
-          </TextStyleTabs>
+          <Text size={'18px'} lineHeight={'25px'}>
+            <FormattedMessage id={'bullets.pluses.descr'}/>
+          </Text>
         </Box>
         <Box width={'280px'}>
-          <Svg3 />
+          <Cost />
           <DivMargin height='30px'/>
-          <Title>
-            {textComponent.adventages.title[2]}
-          </Title>
+          <Text themeText={'sectionTitle'}>
+            <FormattedMessage id={'bullets.cost.title'}/>
+          </Text>
           <DivMargin height='20px'/>
-          <TextStyleTabs>
-            {textComponent.adventages.text[2]}
-          </TextStyleTabs>
+          <Text size={'18px'} lineHeight={'25px'}>
+            <FormattedMessage id={'bullets.cost.descr'}/>
+          </Text>
         </Box>
       </Box>
       <DivMargin height='180px'/>
--- site/ui/bullets/Title.tsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled'
-import { TitleH2 } from '../text/TextStyle'
-import theme from '../theme/Theme'
-
-
-export const Title = styled(TitleH2)(() => ({
-  fontWeight: 500,
-  color: theme.colors.black,
-}))
